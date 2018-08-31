d14fa62f677540b3191d44998434ea60aeee1992
Penguinsama <30609180+Penguinsama@users.noreply.github.com>
2018-08-31 15:06:54 -0700

    Added abstract attribute class & abstract character class

---
+++ src/main/java/AbstractAttribute.java
@@ -0,0 +1,5 @@
+/**
+ * Created by devin.saverline on 8/31/18.
+ */
+public abstract class AbstractAttribute {
+}
---
+++ src/main/java/AbstractCharacter.java
@@ -0,0 +1,6 @@
+/**
+ * Created by devin.saverline on 8/31/18.
+ */
+public abstract class AbstractCharacter {
+
+}
