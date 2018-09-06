1f62b8b0a2b3c6d1a1be3fcfffa4c67d9f2c1c9f
Penguinsama <30609180+Penguinsama@users.noreply.github.com>
2018-09-06 14:16:24 -0700

    more work
    Deleted duplicated classes, added AbstractWhiteWolfStat

--- src/main/java/AbstractCharacter.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by devin.saverline on 8/31/18.
- */
-public abstract class AbstractCharacter {
-
-}
---
+++ src/main/java/Stat/AbstractWhiteWolfStat.java
@@ -0,0 +1,7 @@
+package Stat;
+
+/**
+ * Created by devin.saverline on 9/6/18.
+ */
+public class AbstractWhiteWolfStat extends AbstractStat {
+}
